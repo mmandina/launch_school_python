485cbceb0aef7860892b504b20d5cde3e5ea8135
maxwellmandina <maxwell.mandina@gmail.com>
2024-04-24 20:37:15 -0400

    variables chapter

---
+++ PY100/variables/age.py
@@ -0,0 +1,4 @@
+age = 22
+print(f'You are {age} years old')
+for x in range(10,50,10):
+    print(f'In {x} years, you will be {x+age} years old.')
---
+++ PY100/variables/bank.py
@@ -0,0 +1,12 @@
+principal = 1000.00
+
+balance = principal * (1.05) ** 5
+
+print(balance)
+
+balance = 1000
+
+for x in range(0,5):
+    balance = balance * 1.05
+
+print(balance)
